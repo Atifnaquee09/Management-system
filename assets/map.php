950718a43f3a8da9c016210fca7255260de3c1f5
@Atifnaquee09 <atif123n@gmail.com>
2023-04-29 20:07:46 +0530

    final

---
+++ assets/map.php
@@ -0,0 +1,15 @@
+<section class='map'>
+         <h1>Map</h1>
+        <span class="map_location">
+                <p>Home</p>
+                <i class="fas fa-chevron-right"></i>
+                <p class="all_map_para">Map</p> 
+        </span>
+        <div class="show_map">
+            <h1>School Location</h1>
+            <div class='show_map_inner'>
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3500.1589696468495!2d77.5202959150102!3d28.68489088239718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cf2f0c8f0520d%3A0xff366f002feb9a65!2sDasna%20Toll%20Plaza%2C%20Hapur%20Rd%2C%20Mayur%20Vihar%20Dasna%2C%20Dasna%2C%20Uttar%20Pradesh%20201015!5e0!3m2!1sen!2sin!4v1681086246749!5m2!1sen!2sin" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+         </div>
+         <h2>School System</h2>
+</section>
